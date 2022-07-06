703e5bb15292e91721df3c949b5e6ffa65c7d6d1
JerryBian <JerryBian@outlook.com>
2022-07-06 22:17:39 +0800

    2.3

--- script/build-binary.sh
+++ script/build-binary.sh
@@ -10,6 +10,6 @@ echo commit hash is $GITHASH
 echo Build time is $BUILDTIME
 
 env GOOS=darwin GOARCH=amd64 go build -v -ldflags "-X 'main.AppVer=$VER' -X 'main.BuildTime=$BUILDTIME' -X 'main.GitHash=$GITHASH'" -o _/darwin-amd64/lark *.go 
-env GOOS=linux GOARCH=amd64 go build -v -ldflags "-X 'main.AppVer=$VER' -X 'main.BuildTime=$BUILDTIME' -X 'main.GitHash=$GITHASH'" -o ../_/linux-amd64/lark ../*.go
-env GOOS=linux GOARCH=arm64 go build -v -ldflags "-X 'main.AppVer=$VER' -X 'main.BuildTime=$BUILDTIME' -X 'main.GitHash=$GITHASH'" -o ../_/linux-arm64/lark ../*.go
-env GOOS=windows GOARCH=amd64 go build -v -ldflags "-X 'main.AppVer=$VER' -X 'main.BuildTime=$BUILDTIME' -X 'main.GitHash=$GITHASH'" -o ../_/windows-amd64/lark.exe ../*.go
+env GOOS=linux GOARCH=amd64 go build -v -ldflags "-X 'main.AppVer=$VER' -X 'main.BuildTime=$BUILDTIME' -X 'main.GitHash=$GITHASH'" -o _/linux-amd64/lark *.go
+env GOOS=linux GOARCH=arm64 go build -v -ldflags "-X 'main.AppVer=$VER' -X 'main.BuildTime=$BUILDTIME' -X 'main.GitHash=$GITHASH'" -o _/linux-arm64/lark *.go
+env GOOS=windows GOARCH=amd64 go build -v -ldflags "-X 'main.AppVer=$VER' -X 'main.BuildTime=$BUILDTIME' -X 'main.GitHash=$GITHASH'" -o _/windows-amd64/lark.exe *.go
